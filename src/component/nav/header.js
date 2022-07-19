a33bd32f5ccc6792950e1adf89025f03febefa5d
ajith shah <ajithshah37@gmail.com>
2022-07-19 15:06:30 +0530

    [Ajith] Navbar fix

---
+++ src/component/nav/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () =>
+  <div className="topnav">
+    <a className="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>;
+
+export default Header;
