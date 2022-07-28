3e16bb647d3c3f2300fc42ae55a9e92edae233f7
ajith shah <ajithshah37@gmail.com>
2022-07-28 13:36:33 +0530

    [Ajith] Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
-import NavigationBar from './component/Navbar/navbar.js';
-import Main from './component/Main/main.js';
+import NavigationBar from './component/Navbar/navbar.jsx';
+import Main from './component/Main/main.jsx';
+import EventBind from './component/Event/Event.jsx';
 function App() {
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
       <Main></Main>
+      <EventBind/>
     </div>
   );
 }
